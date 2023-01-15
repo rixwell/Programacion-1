45f1ad1a0b8688753d22f878e3208ac5868d0b5b
Richard Almengo <87624842+rixwell@users.noreply.github.com>
2023-01-15 15:32:58 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int n, n1, n2, suma;
+
+Console.WriteLine("Ingrese un numero de dos digitos:");
+
+n = Convert.ToInt32(Console.ReadLine());
+
+n1 = n % 10;
+n = n / 10;
+n2 = n % 10;
+n = n / 10;
+
+suma = n1 + n2;
+
+Console.WriteLine ("El resultado de la suma es: " + suma );
+
+Console.ReadKey();
+
+
