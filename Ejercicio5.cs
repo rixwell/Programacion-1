3dcf8d871a3fab07af92696b8f6ca78fb70231ff
Richard Almengo <87624842+rixwell@users.noreply.github.com>
2023-01-15 19:02:02 -0400

    Add files via upload

---
+++ Ejercicio5.cs
@@ -0,0 +1,30 @@
+﻿int n, n1, n2, n3;
+
+Console.WriteLine("Digite un numero entero de tres digitos: ");
+n = Convert.ToInt32(Console.ReadLine());
+n3 = n % 10;
+n = n / 10;
+n2 = n % 10;
+n = n / 10;
+n1 = n % 10;
+
+
+
+            if (n1 > n2 && n1 > n3)
+            {
+            
+                
+                    Console.WriteLine("{0} es el digito mayor y esta en la primera posicion", n1);
+                
+            }
+            else if (n2 > n1 && n2 > n3)
+            {
+                
+                    Console.WriteLine("{0} es el digito mayor y esta en la segunda posicion", n2);
+            }
+                else{
+
+                Console.WriteLine("{0} es el digito mayor y esta en la tercera posicion", n3);
+                }
+            Console.ReadKey();
+        
