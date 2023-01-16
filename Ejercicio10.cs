14cf88a2c875548bc09b92320c982facb589a16a
Richard Almengo <87624842+rixwell@users.noreply.github.com>
2023-01-15 20:39:03 -0400

    Add files via upload

---
+++ Ejercicio10.cs
@@ -0,0 +1,32 @@
+﻿ int n1, n2, nmayor, nmenor;
+
+ Console.WriteLine("Digite el primer numero: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite el segundo numero: ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+            
+            if (n1 > n2)
+            {
+                nmayor = n1;
+                nmenor = n2;
+            }
+            else
+            {
+                nmayor = n2;
+                nmenor = n1;
+            }
+            if ((nmayor - nmenor) <= 10)
+            {
+                Console.WriteLine("Los numeros se encuentran organizados en la siguiente secuencia: ");
+                for (int i = nmenor; i <= nmayor; i++)
+                {
+                    Console.WriteLine(i);
+                }
+                
+            } 
+            else 
+            {
+                Console.WriteLine("Existe una diferencia entre ambos numeros mayor a diez. ");
+            }
+            Console.ReadKey();
