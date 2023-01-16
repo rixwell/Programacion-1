e266e2078453e677180c26ec05e35cb97677d07e
Richard Almengo <87624842+rixwell@users.noreply.github.com>
2023-01-15 20:12:39 -0400

    Add files via upload

---
+++ Ejercicio7.cs
@@ -0,0 +1,16 @@
+﻿int n1, n2, n3, nm;
+
+            Console.WriteLine("Ingrese el primer numero:");
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo numero:");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el tercer numero:");
+            n3 = Convert.ToInt32(Console.ReadLine());
+
+            nm = Math.Max(n1, n2);
+            nm = Math.Max(nm, n3);
+
+            Console.WriteLine("El número mayor es: " + nm);
+            Console.ReadKey();
