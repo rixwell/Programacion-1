b6f3c4d928211da0c081604fa122065f4eb5cb40
Richard Almengo <87624842+rixwell@users.noreply.github.com>
2023-01-15 20:32:02 -0400

    Add files via upload

---
+++ Ejercicio9.cs
@@ -0,0 +1,16 @@
+﻿ int n, penultimodig,segdigito;
+ Console.WriteLine("Ingrese un numero de cuatrp digitos: ");
+            n= Convert.ToInt32(Console.ReadLine());
+
+            segdigito = (n / 100) % 10;
+            penultimodig = (n / 10) % 10;
+
+            if (segdigito == penultimodig)
+            {
+                Console.WriteLine("El segundo digito es igual al penultimo digito ");
+            }
+            else
+            {
+                Console.WriteLine("El segundo digito no es igual al penultimo digito ");
+            }
+            Console.ReadKey();
